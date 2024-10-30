d3225308c540d9e3e38d237f67e95da9cdcef23e
Moeez Rashid <142141114+031445@users.noreply.github.com>
2024-10-30 18:18:41 +0500

    Create App.jsx

---
+++ List in react/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import List from "./components/list rendring through API"
+
+const App =()=>{
+  return(
+    <>
+    <List />
+    
+    
+    </>
+  )
+}
+export default App
